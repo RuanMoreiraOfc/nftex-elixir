ca647146def50ff31b1c31d12e5abd09a80d850d
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-10-22 17:59:11 -0300

    chore(ArtsFace): delegate `list` method

--- lib/nftex/arts.ex
+++ lib/nftex/arts.ex
@@ -1,6 +1,7 @@
 defmodule Nftex.Arts do
-  alias Nftex.Arts.{Create, Get}
+  alias Nftex.Arts.{Create, Get, List}
 
+  defdelegate list(), to: List, as: :call
   defdelegate get(id), to: Get, as: :by_id
   defdelegate create(params), to: Create, as: :call
 end
